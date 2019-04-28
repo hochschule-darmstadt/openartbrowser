44f90b7c2c4c361fe6b7048553f3267f3b36f850
Jannik Schormann <jannik.schormann@stud.h-a.de>
2019-04-29 00:02:48 +0200

    renamed some methods in artist component, added ngIfs to only show non empty fields in artist information, changed influencedBy to influenced_by in artist model

--- app/src/app/shared/components/slider/slider.component.ts
+++ app/src/app/shared/components/slider/slider.component.ts
@@ -10,34 +10,31 @@ export class SliderComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-     /** Cuts big array into smaller arrays */
-     this.spliceArray(this.items);
+    /** Cuts big array into smaller arrays */
+    this.buildSliderPages();
   }
 
   splicedItems: Array<Entity[]> = [];
 
   @Input()
-  items: Entity[];
+  items: Entity[] = [];
 
   @Input()
   heading: string;
 
-   /** Cut array of artworks in multiple arrays each with 8 items 
+  /** Cut array of artworks in multiple arrays each with 8 items
    * arrayNumber : how many arrays with 8 items
    * sliderItems: Array<Entity[]> = [];  Is Array of arrays with 8 items each
    * temporaryArray   one array with 8 items
-  */
-  sliderItems: Array<Entity[]> = [];
-
-  spliceArray(arrayToSplice: Entity[]){ 
+   */
+  buildSliderPages() {
     let temporaryArray: Entity[] = [];
     let arrayNumber: number = 0;
-    // There are 8 images on each slide. 
-    arrayNumber = arrayToSplice.length / 8;
-    for (let i = 0; i < arrayNumber; i++ ){   
-      temporaryArray = arrayToSplice.splice(0,8);
-      this.splicedItems.push(temporaryArray);      
+    // There are 8 images on each slide.
+    arrayNumber = this.items.length / 8;
+    for (let i = 0; i < arrayNumber; i++) {
+      temporaryArray = this.items.splice(0, 8);
+      this.splicedItems.push(temporaryArray);
     }
   }
-
 }

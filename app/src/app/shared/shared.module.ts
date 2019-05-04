c70bfa8693590a4cc8149804407797f47a3b5082
Marina Reichert <shil7en@gmail.com>
2019-05-04 14:10:44 +0200

    Added RouterModule

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SliderComponent } from './components/slider/slider.component';
 import { SliderItemComponent } from './components/slider/slider-item/slider-item.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule} from '@angular/router';
 
 /** Everything that should be used within multiple feature modules but isn't always required goes here */
 @NgModule({
   declarations: [SliderComponent, SliderItemComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, RouterModule],
   exports: [SliderComponent],
 })
 export class SharedModule {}

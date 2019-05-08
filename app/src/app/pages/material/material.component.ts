7f2937a2c2e942a2cf0904611766265219212c0b
Jannik Schormann <jannik.schormann@stud.h-a.de>
2019-05-08 09:28:34 +0200

    fixed url subscription in genre and material component

--- app/src/app/pages/material/material.component.ts
+++ app/src/app/pages/material/material.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from 'src/app/core/services/data.service';
 import { ActivatedRoute } from '@angular/router';
-import { take } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { Material, Entity } from 'src/app/shared/models/models';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-material',
   templateUrl: './material.component.html',
   styleUrls: ['./material.component.scss'],
 })
-export class MaterialComponent implements OnInit {
+export class MaterialComponent implements OnInit, OnDestroy {
+  /** use this to end subscription to url parameter in ngOnDestroy */
+  private ngUnsubscribe = new Subject();
+
+  /** The entity this page is about */
+  material: Material = null;
+
   constructor(private dataService: DataService, private route: ActivatedRoute) {}
 
   /** hook that is executed at component initialization */
   ngOnInit() {
     /** Extract the id of entity from URL params. */
-    this.route.paramMap.pipe(take(1)).subscribe((params) => {
+    this.route.paramMap.pipe(takeUntil(this.ngUnsubscribe)).subscribe((params) => {
       const materialId = params.get('materialId');
       /** Use data service to fetch entity from database */
       this.dataService
@@ -29,16 +36,17 @@ export class MaterialComponent implements OnInit {
     });
   }
 
-  /** The entity this page is about */
-  material: Material = null;
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
   sliderItems: Entity[] = [
     {
       id: '',
       description: '',
       type: '',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/a/a2/Stube_im_Sonnenlicht_-_Carl_Knauf.jpg',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Stube_im_Sonnenlicht_-_Carl_Knauf.jpg',
       label: 'Living Room in Sunlight',
     },
     {
@@ -60,37 +68,38 @@ export class MaterialComponent implements OnInit {
       id: '',
       description: '',
       type: '',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/f/fb/Le_Bosphore_F%C3%A9lix_Ziem.jpg',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Le_Bosphore_F%C3%A9lix_Ziem.jpg',
       label: 'The Bosphorus',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Van_Emelen%2C_Adrien_Henri_Vital_-_Negro_com_Chap%C3%A9u_e_Cachimbo.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/3/3c/Van_Emelen%2C_Adrien_Henri_Vital_-_Negro_com_Chap%C3%A9u_e_Cachimbo.jpg',
       label: 'Negro com Chapeu e Cachimbo',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Van_Emelen%2C_Adrien_Henri_Vital_-_%C3%8Dndio_de_Perfil.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/c/c9/Van_Emelen%2C_Adrien_Henri_Vital_-_%C3%8Dndio_de_Perfil.jpg',
       label: 'Ãndio de Perfil',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Van_Emelen%2C_Adrien_Henri_Vital_-_Negrinho_sob_uma_torneira.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/0/01/Van_Emelen%2C_Adrien_Henri_Vital_-_Negrinho_sob_uma_torneira.jpg',
       label: 'Negrinho sob uma torneira',
     },
     {
       id: '',
       description: '',
       type: '',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/e/ed/Vilho_Lampi_-_Self-Portrait.jpg',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Vilho_Lampi_-_Self-Portrait.jpg',
       label: 'Self-Portrait',
     },
     {
@@ -112,7 +121,8 @@ export class MaterialComponent implements OnInit {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Van_Emelen%2C_Adrien_Henri_Vital_-_Dois_Negrinhos_com_Chap%C3%A9u.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/a/a0/Van_Emelen%2C_Adrien_Henri_Vital_-_Dois_Negrinhos_com_Chap%C3%A9u.jpg',
       label: 'Dois Negrinhos com ChapÃ©u',
     },
     {
@@ -133,35 +143,40 @@ export class MaterialComponent implements OnInit {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/6/60/Karol_Hiller_-_Deska_0_%2C_Kompozycja_0_-_Google_Art_Project.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/6/60/Karol_Hiller_-_Deska_0_%2C_Kompozycja_0_-_Google_Art_Project.jpg',
       label: 'Deska 0 / Kompozycja 0',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Van_Emelen%2C_Adrien_Henri_Vital_-_Negro_com_Chap%C3%A9u.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Van_Emelen%2C_Adrien_Henri_Vital_-_Negro_com_Chap%C3%A9u.jpg',
       label: 'Negro com ChapÃ©u',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Henry_Ossawa_Tanner_-_Gateway%2C_Tangier_%28Smithsonian_American_Art_Museum%29.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/9/9e/Henry_Ossawa_Tanner_-_Gateway%2C_Tangier_%28Smithsonian_American_Art_Museum%29.jpg',
       label: 'Gateway, Tangier',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Van_Emelen%2C_Adrien_Henri_Vital_-_Homem_com_Chap%C3%A9u%2C_Camisa_Branca.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/b/b6/Van_Emelen%2C_Adrien_Henri_Vital_-_Homem_com_Chap%C3%A9u%2C_Camisa_Branca.jpg',
       label: 'Homem com ChapÃ©u, Camisa Branca',
     },
     {
       id: '',
       description: '',
       type: '',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/6/65/Van_Emelen%2C_Adrien_Henri_Vital_-_Negro_com_Chap%C3%A9u_Tocando_Viol%C3%A3o.jpg',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/6/65/Van_Emelen%2C_Adrien_Henri_Vital_-_Negro_com_Chap%C3%A9u_Tocando_Viol%C3%A3o.jpg',
       label: 'Negro com ChapÃ©u Tocando ViolÃ£o',
     },
   ];

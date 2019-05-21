274f7b8d9aec99aeec4f7951d205ed28efb03f95
Bagus Nanda <baguszz@gmail.com>
2019-05-21 12:56:03 +0200

    add image viewer to modal

--- app/src/app/pages/artwork/artwork.routing.module.ts
+++ app/src/app/pages/artwork/artwork.routing.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ArtworkComponent } from './artwork.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ImageViewerModule } from "ngx-image-viewer";
 
-import { NgxImageZoomModule } from 'ngx-image-zoom';
 
 const artworkRoutes: Routes = [
   {
@@ -15,7 +15,7 @@ const artworkRoutes: Routes = [
 
 @NgModule({
   declarations: [ArtworkComponent],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(artworkRoutes), NgxImageZoomModule.forRoot()],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(artworkRoutes), ImageViewerModule.forRoot()],
   exports: [ArtworkComponent],
 })
 export class ArtworkRoutingModule {}

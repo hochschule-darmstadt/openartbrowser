b58e87cf98e146a14694f3836b3c26c0e9999b5f
Bagus Nanda <baguszz@gmail.com>
2019-05-29 22:29:19 +0200

    fix carousel cycle, shuffle categories (fit image size), background image

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { NgbCarouselConfig, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { Entity, Artist, Artwork, Movement, Material, Location, Genre, Motif } from 'src/app/shared/models/models';
 import { DataService } from 'src/app/core/services/data.service';
 
@@ -19,6 +19,20 @@ export interface SliderCategory {
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+
+  /**
+   * @description all 7 carousels in the page. 
+   * @type {QueryList<NgbCarousel>}
+   * @memberof HomeComponent
+   */
+  @ViewChildren(NgbCarousel) carousel: QueryList<NgbCarousel>;
+
+  /**
+   * @description random url image for the page background 
+   * @type {string}
+   * @memberof HomeComponent
+   */
+  backgroundImageUrl: string;
 
   /**
    * @description variable of object to be fetch in html
@@ -72,13 +86,18 @@ export class HomeComponent implements OnInit {
 
   constructor(public dataService: DataService, ngb_config: NgbCarouselConfig) {
     /** set configuration of ngbCarousel */
-    ngb_config.pauseOnHover = false;
     ngb_config.interval = 10000;
+    ngb_config.keyboard = false;
+    ngb_config.pauseOnHover = false;
   }
 
   ngOnInit() {
-    this.getCategoryItems();
-
+    this.getCategoryItems().then(() => {
+      setTimeout(() => {
+        this.refreshCarousel(); // need to have a bit delay of time (last categories that is fetched is not refreshing carousel cycle)
+      }, 500);
+      this.getRandomImage();
+    });
   }
 
   /**
@@ -86,12 +105,50 @@ export class HomeComponent implements OnInit {
    * @memberof HomeComponent
    */
   getCategoryItems = async (): Promise<void> => {
-    this.upperCategories.artwork.items = await this.dataService.get10CategoryItems<Artwork>('artwork');
-    this.upperCategories.artist.items = await this.dataService.get10CategoryItems<Artist>('artist');
-    this.upperCategories.movement.items = await this.dataService.get10CategoryItems<Movement>('movement');
-    this.lowerCategories.location.items = await this.dataService.get10CategoryItems<Location>('location');
-    this.lowerCategories.material.items = await this.dataService.get10CategoryItems<Material>('material');
-    this.lowerCategories.genre.items = await this.dataService.get10CategoryItems<Genre>('genre');
-    this.lowerCategories.motif.items = await this.dataService.get10CategoryItems<Motif>('object');
+    this.upperCategories.artwork.items = this.shuffle(await this.dataService.get20CategoryItems<Artwork>('artwork'));
+    this.upperCategories.artist.items = this.shuffle(await this.dataService.get20CategoryItems<Artist>('artist'));
+    this.upperCategories.movement.items = this.shuffle(await this.dataService.get20CategoryItems<Movement>('movement'));
+    this.lowerCategories.location.items = this.shuffle(await this.dataService.get20CategoryItems<Location>('location'));
+    this.lowerCategories.material.items = this.shuffle(await this.dataService.get20CategoryItems<Material>('material'));
+    this.lowerCategories.genre.items = this.shuffle(await this.dataService.get20CategoryItems<Genre>('genre'));
+    this.lowerCategories.motif.items = this.shuffle(await this.dataService.get20CategoryItems<Motif>('object'));
+  };
+
+  /**
+   * @description shuffle the items' categories.
+   * @memberof HomeComponent
+   */
+  shuffle = (a: Entity[]): Entity[] => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  };
+
+  /**
+   * @description make auto next carousel works after fetching categories.
+   * @memberof HomeComponent
+   */
+  refreshCarousel = (): void => {
+    this.carousel.forEach((item) => {
+      item.cycle();
+    });
+  };
+
+  /**
+   * @description assign backgroundImageUrl with a random image from one of the items.
+   * @memberof HomeComponent
+   */
+  getRandomImage = (): void => {
+    let allItems: Entity[] = this.upperCategories['artwork'].items.concat(
+      this.upperCategories['artist'].items,
+      this.upperCategories['movement'].items,
+      this.lowerCategories['location'].items,
+      this.lowerCategories['material'].items,
+      this.lowerCategories['genre'].items,
+      this.lowerCategories['motif'].items
+    );
+    this.backgroundImageUrl = allItems[Math.floor(Math.random() * (allItems.length + 1))].image;
   };
 }

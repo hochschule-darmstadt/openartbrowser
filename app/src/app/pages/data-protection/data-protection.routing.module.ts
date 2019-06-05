7b88f8e5f329976a0f3d1fa0f7490e4a2a8da565
Marina Reichert <shil7en@gmail.com>
2019-06-05 13:13:29 +0200

    added about and data-protection components with routing modules

---
+++ app/src/app/pages/data-protection/data-protection.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+import { DataProtectionComponent } from './data-protection.component';
+
+const dataProtectionRoutes: Routes = [
+    {
+        path: '',
+        component: DataProtectionComponent,
+    },
+];
+
+@NgModule({
+    declarations: [DataProtectionComponent],
+    imports: [CommonModule, SharedModule, RouterModule.forChild(dataProtectionRoutes)],
+    exports: [DataProtectionComponent],
+})
+export class DataProtectionRoutingModule { }

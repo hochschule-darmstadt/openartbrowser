e3ea654e6f76059e56dfa2dd7ca86a4baee0abb6
Bagus Nanda <baguszz@gmail.com>
2019-06-05 10:33:19 +0200

    add router module for footer test file

--- app/src/app/core/components/footer/footer.component.spec.ts
+++ app/src/app/core/components/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { RouterModule } from '@angular/router';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +9,12 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [ 
+        FooterComponent 
+      ]
     })
     .compileComponents();
   }));

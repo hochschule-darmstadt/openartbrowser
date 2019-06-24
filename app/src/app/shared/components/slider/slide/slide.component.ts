3e7286389adf3802b53a33987b499ef67d25c395
Jannik Schormann <jannik.schormann@stud.h-a.de>
2019-06-24 10:12:14 +0200

    images which failed loading are removed from slider, slider image loading improved : when a slide becomes visible not only the images of this slide, but also the previous and the next slide are loaded, remove fixed height from slider to fix mobile appearance

--- app/src/app/shared/components/slider/slide/slide.component.ts
+++ app/src/app/shared/components/slider/slide/slide.component.ts
@@ -1,34 +1,31 @@
-import { Component, Input, Output, EventEmitter, AfterViewInit, ElementRef, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, ElementRef } from '@angular/core';
 import { Entity } from 'src/app/shared/models/models';
 import { DataService } from 'src/app/core/services/data.service';
+import { Slide } from '../slider.component';
 
+/**
+ * a slide of the slider.
+ * if the image of an item cannot be loaded due to invalid url, the item is removed from the slider
+ * and all following items move one position to the left to fill the gap.
+ */
 @Component({
   selector: 'app-slide',
   templateUrl: './slide.component.html',
   styleUrls: ['./slide.component.scss'],
 })
-export class SlideComponent implements AfterViewInit, OnChanges {
-  /** whether slide images should be loaded */
-  public loadContent = false;
+export class SlideComponent implements AfterViewInit {
+  /** the slide that should be displayed */
+  @Input() slide: Slide;
 
-  /**
-   * @description Input: the items being passed.
-   */
-  @Input() items: Entity[];
-
-  /**
-   * @description emits hovered artwork on item hover event
-   */
+  /** emits hovered artwork on item hover event */
   @Output()
   itemHover: EventEmitter<Entity> = new EventEmitter<Entity>();
 
-  constructor(public dataService: DataService, private el: ElementRef) { }
+  /** trigger deletion of unused slides in slider component */
+  @Output()
+  deleteUnusedSlides: EventEmitter<void> = new EventEmitter<void>();
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.items) {
-      console.log(this.items);
-    }
-  }
+  constructor(public dataService: DataService, private el: ElementRef) {}
 
   ngAfterViewInit() {
     if (window && 'IntersectionObserver' in window) {
@@ -37,7 +34,14 @@ export class SlideComponent implements AfterViewInit, OnChanges {
           /** check whether any element of the slide is currently visible on screen */
           entries.forEach(({ isIntersecting }) => {
             if (isIntersecting) {
-              this.loadContent = true;
+              /** enable image loading for this slide, the next slide and the previous slide if slide is visible */
+              this.slide.loadContent = true;
+              if (this.slide.nextSlide) {
+                this.slide.nextSlide.loadContent = true;
+              }
+              if (this.slide.prevSlide) {
+                this.slide.prevSlide.loadContent = true;
+              }
               obs.unobserve(this.el.nativeElement);
             }
           });
@@ -46,7 +50,52 @@ export class SlideComponent implements AfterViewInit, OnChanges {
       );
       obs.observe(this.el.nativeElement);
     } else {
-      this.loadContent = true;
+      this.slide.loadContent = true;
     }
+  }
+
+  /** if image of an item could not be loaded, shift succeeding items from this slide and following slides
+   * one position to the left. if the last slide becomes empty during this process, delete the last slide.
+   * @param item the item whose image could not be loaded
+   **/
+  onLoadingError(item: Entity) {
+    const removeIndex = this.slide.items.findIndex((i) => {
+      return i.id === item.id;
+    });
+
+    this.slide.items.splice(removeIndex, 1);
+
+    const newItem = this.shiftItemForward(this.slide.nextSlide);
+    if (newItem) {
+      this.slide.items.push(newItem);
+    }
+
+    if (this.slide.items.length === 0) {
+      this.deleteUnusedSlides.emit();
+    }
+  }
+
+  /**
+   * removes the first item from this slide and shift succeeding items from this slide and following slides
+   * one position to the left.
+   * @param slide slide of which first item should be shifted to the previous slide
+   */
+  shiftItemForward(slide: Slide) {
+    if (!slide) {
+      return null;
+    }
+
+    if (!slide.isLastSlide) {
+      const newItem = this.shiftItemForward(slide.nextSlide);
+      if (newItem) {
+        slide.items.push(newItem);
+      }
+    }
+
+    const returnedItem = slide.items.splice(0, 1)[0];
+    if (slide.items.length === 0) {
+      this.deleteUnusedSlides.emit();
+    }
+    return returnedItem;
   }
 }
--- app/src/app/shared/components/slider/slider.component.ts
+++ app/src/app/shared/components/slider/slider.component.ts
@@ -1,56 +1,45 @@
-import { Component, Input, SimpleChanges, OnChanges, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges, EventEmitter, Output } from '@angular/core';
 import { Entity } from '../../models/models';
 
+export interface Slide {
+  /** artworks displayed on this slide */
+  items: Entity[];
+  /** previous slide of slider */
+  prevSlide: Slide;
+  /** next slide of slider */
+  nextSlide: Slide;
+  /**  whether slide images should be loaded */
+  loadContent: boolean;
+  /** flag if this is the last slide of the slider */
+  isLastSlide: boolean;
+  /** flag if this is the first slide of the slider */
+  isFirstSlide: boolean;
+  /** slide id to uniquely identify slide within this slider */
+  id: number;
+}
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent implements OnInit, OnChanges {
+export class SliderComponent implements OnChanges {
+
+  /** title of this slider */
   @Input() heading: string;
 
-  /**
-   * @description Input: the items being passed.
-   */
+  /**  entities that should be displayed in this slider */
   @Input() items: Entity[] = [];
 
-  /**
-   * @description slides of the slider, max 8 items each.
-   */
-  //slides: Array<Entity[]> = [[]];
+  // slides of the slider, max 8 items each.
+  slides: Slide[];
 
-  slides = [];
-
-
-
-  ngOnInit() {
-    // setTimeout(() => {
-    //   this.slides = [];
-    //   for (let i = 0; i < this.itemsAsPointers.length; i += 8) {
-    //     this.slides.push(this.itemsAsPointers.slice(i, i + 8));
-    //   }
-    //   console.log(this.slides[0][0].item);
-    //   setTimeout(() => {
-    //     this.itemsAsPointers[0].item = undefined;
-
-    //     console.log(this.slides[0][0].item);
-    //   }, 4000);
-
-    // }, 2000);
-
-  }
-
-  /**
-   * @description emits hovered artwork on hover event.
-   */
+  /** emits hovered artwork on hover event. */
   @Output() itemHover: EventEmitter<Entity> = new EventEmitter<Entity>();
 
   constructor() { }
 
-  /**
-   * @description Hook that is called when any data-bound property of a directive changes.
-   */
+  /** Hook that is called when any data-bound property of a directive changes. */
   ngOnChanges(changes: SimpleChanges) {
     // rebuild slides if slider items input changed.
     if (changes.items && this.items) {
@@ -58,17 +47,60 @@ export class SliderComponent implements OnInit, OnChanges {
     }
   }
 
-  /**
-   * @description Divide the slider items into slides.
-   */
+  /** Divide the slider items into slides. Initialize slides. */
   buildSlides(): void {
-    const slidesBuilt: Array<Entity[]> = [];
+    const slidesBuilt: Slide[] = [];
     // There are 8 images on each slide.
     const numberOfSlides = this.items.length / 8;
     for (let i = 0; i < numberOfSlides; i++) {
-      const slide: Entity[] = this.items.slice(i * 8, i * 8 + 8);
+      // get next 8 items out of items array
+      const items: Entity[] = this.items.slice(i * 8, i * 8 + 8);
+
+      const slide: Slide = {
+        items,
+        prevSlide: null,
+        nextSlide: null,
+        loadContent: false,
+        isFirstSlide: false,
+        isLastSlide: false,
+        id: i,
+      };
+
+      if (i === numberOfSlides - 1) {
+        slide.isLastSlide = true;
+      }
+      if (i === 0) {
+        slide.isFirstSlide = true;
+      }
+
+      /** set pointers between this slide and previous slide  */
+      if (i !== 0) {
+        slide.prevSlide = slidesBuilt[i - 1];
+        slidesBuilt[i - 1].nextSlide = slide;
+      }
+
+      /** if this is the last slide, also set pointers between first and last slide  */
+      if (i === numberOfSlides - 1 && numberOfSlides !== 1) {
+        slide.nextSlide = slidesBuilt[0];
+        slidesBuilt[0].prevSlide = slide;
+      }
+
       slidesBuilt.push(slide);
     }
     this.slides = slidesBuilt;
+  }
+
+  /** delete slide based on id of the passed slide 
+   * @param slide slide to be deleted
+  */
+  deleteUnusedSlides() {
+    const lastSlide = this.slides[this.slides.length - 1];
+    if (lastSlide.items.length === 0) {
+      if (!lastSlide.isFirstSlide) {
+        lastSlide.prevSlide.isLastSlide = true;
+        lastSlide.prevSlide.nextSlide = null;
+      }
+      this.slides.splice(this.slides.length - 1, 1);
+    }
   }
 }

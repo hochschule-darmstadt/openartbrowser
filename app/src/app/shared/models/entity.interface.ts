7ff8df2f342be51435479b901fbd6e220bc3969b
Marcel Moravek <marcel.moravek@stud.h-da.de>
2019-11-30 14:59:26 +0100

    split models in own files

---
+++ app/src/app/shared/models/entity.interface.ts
@@ -0,0 +1,14 @@
+
+export interface Entity {
+    id: string;
+    label?: string;
+    description?: string;
+    abstract?: string;
+    wikipediaLink?: string;
+    image?: string;
+    imageSmall?: string;
+    imageMedium?: string;
+    type: string;
+    absoluteRank: number;
+    relativeRank: number;
+}

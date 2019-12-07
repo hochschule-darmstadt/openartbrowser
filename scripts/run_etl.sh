6dd70d29d644054abb4fe67b1468ab94eb647d32
Tilo Michel <tilo.w.michel@stud.h-da.de>
2019-12-07 13:39:09 +0100

    Adjusted etl script and renamings
    - Also adjusted console output
    - Moved user-config.py to script directory
    so there is no export pywiki environment
    variable needed

--- scripts/run_etl.sh
+++ scripts/run_etl.sh
@@ -16,6 +16,9 @@ node ../../../data_manipulation/script_motifs_rank.js
 # Merges all *_rank.json files into art_ontology.json
 node ../../../data_manipulation/script_flatten_rank.js
 
+# Move the generated art_ontology.json to the directory crawler_output
+mv art_ontology.json ../../../crawler_output/art_ontology.json
+
 python3 ../../../data_manipulation/script_add_youtube_videos.py
 
 python3 ../../../Wikidata\ crawler/split_languages.py

89f422732e8eca7ad74e7cdabdb369f728d3d2a0
Tilo Michel <tilo.w.michel@stud.h-da.de>
2019-12-07 17:33:27 +0100

    Just use one requirements.txt for etl_install

--- scripts/install_etl.sh
+++ scripts/install_etl.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-pip3 install -r Wikidata\ crawler/requirements.txt
-pip3 install -r data_manipulation/requirements.txt
+pip3 install -r requirements.txt
 cd data_manipulation
 npm install
-cd ..
-pip3 install -r upload_to_elasticsearch/requirements.txt

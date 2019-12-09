02be4deb0ca81537dcfd16eca5f4739407c56a5d
lbucas <luc.b1197@gmail.com>
2019-12-09 16:54:24 +0100

    Add GAnaylitcs data privacy entry

--- app/src/app/pages/data-protection/data-protection.component.ts
+++ app/src/app/pages/data-protection/data-protection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AnalyticsProperty} from '../../../environments/environment_analytics';
 
 @Component({
   selector: 'app-data-protection',
@@ -10,5 +11,12 @@ export class DataProtectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  gaOptout() {
+    const disableStr = 'ga-disable-' + AnalyticsProperty.trackingId;
+    document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    window[disableStr] = true;
+    alert('Das Tracking ist jetzt deaktiviert');
+  }
 
 }

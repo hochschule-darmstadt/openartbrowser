036bc8553bb97c532121f004c333cfabbd5882a0
Jamie John Armstrong-Arndt <JamieArmstrongArndt@googlemail.com>
2019-12-11 14:31:01 +0100

    Adding type-input instead of icon, small changes to sass-file of the badge and trimming of content

--- app/src/app/shared/components/badge/badge.component.ts
+++ app/src/app/shared/components/badge/badge.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { Component, ViewEncapsulation, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-badge',
@@ -7,7 +7,7 @@ import { Component, ViewEncapsulation, Input } from '@angular/core';
   styleUrls: ['./badge.component.scss']
 })
 
-export class BadgeComponent {
+export class BadgeComponent implements OnInit {
 
   /** the text that is shown in the badge */
   @Input() label: string = 'Add a custom label';
@@ -16,9 +16,27 @@ export class BadgeComponent {
   @Input() tooltip: string = 'Insert your tooltip here';
 
   /** the font awesome icon class */
-  @Input() icon: string = 'fa-wind';
+  @Input() type: string = 'movement';
 
   /** the url to which the badge should redirect when being clicked */
   @Input() redirectUrl: string = '/';
 
+  icon: string;
+
+  ngOnInit() {
+    this.icon = icons[this.type];
+    if (this.tooltip && this.tooltip.length) this.tooltip = this.tooltip.trim().substring(0, this.tooltip.indexOf('.', 150) + 1) + ' [...]'
+  }
+
+}
+
+enum icons {
+  artist = "fa-user",
+  influencer = "fa-user",
+  artwork = "fa-image",
+  movement = "fa-wind",
+  location = "fa-archway",
+  motif = "fa-image",
+  genre = "fa-tag",
+  material = "fa-scroll"
 }

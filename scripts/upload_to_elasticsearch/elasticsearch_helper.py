b7fa9b2001114af36245bd83b7a337dac2ec51fe
Tilo Michel <tilo.w.michel@stud.h-da.de>
2019-12-15 17:35:36 +0100

    Adjustments for swapping indices
    #119
    Added different convenience functions.

--- scripts/upload_to_elasticsearch/elasticsearch_helper.py
+++ scripts/upload_to_elasticsearch/elasticsearch_helper.py
@@ -9,116 +9,286 @@ import requests
 import sys
 import csv
 
-def create_or_update_index(
-    index_name,
-    repository_name='openartbrowser_index_backup',
-    backup_directory='/var/lib/elasticsearch/backup',
-    file=Path(__file__).resolve().parent.parent / "crawler_output" / "art_ontology.json"):
-    """
-    Creates or updates an index with new documents. 
-    If the passed index exists a snapshot for the current index is created.
 
-    :arg index_name: Index name in which documents should be created or updated
-                     The openartbrowser index is called 'api'
-    :arg repository_name: Name for a repository which stores snapshots
-                          The openartbrowser repository is 'openartbrowser_index_backup'
-    :arg backup_directory: Directory in which the repository is located
-                          The openartbrowser backup directory is /var/lib/elasticsearch/backup
-                          IMPORTANT: 
-                          1. The directory has to exist before execution
-                          2. This directory is also needed in the elasticsearch.yaml configuration file
-                                - Following entry in elasticsearch.yml required:
-                                  path.repo: ["path_to_folder"]
-    :arg file: Name of the file which contains the documents to be created or updated
+def create_empty_index(
+    index_name
+) -> bool:
+    """
+    Creates an empty index (meaning no documents inside)
+
+    :arg index_name: Name of the index to be created
+
+    Returns:
+        True if index didn't exist and could be created else False
+    """
+    es = Elasticsearch()
+
+    if es.indices.exists(index=index_name):
+        print("Index with the name " + index_name + " already exists")
+        return False
+    es.indices.create(index=index_name)
+    return True
+
+
+def delete_index(index_name) -> bool:
+    """
+    Delete an index by it's name
+
+    :arg index_name: Name of the index to be deleted
+
+    Returns:
+        True if index extists and could be deleted else False
+
+    """
+    es = Elasticsearch()
+    if es.indices.exists(index=index_name):
+        print("Deleting index: " + index_name + " now")
+        es.indices.delete(index_name)
+        return True
+    print("The index: " + index_name + "to be deleted doesn't exist")
+    return False
+
+
+def create_index(
+    index_name,
+    file) -> None:
+    """
+    Creates an index with new documents from art_ontology_<language_code>.json
+
+    :arg index_name: Index name in which documents should be created
+    :arg file: Name of the file which contains the documents to be created 
+               e. g. art_ontology_<language_code>.json
     """
     # Uses localhost:9200 (elasticsearch default) to create the index with it's documents
     es = Elasticsearch()
-    print("Start creating/updating the index \"" + index_name +
+    print("Start creating the index \"" + index_name +
         "\" now. Current time: " + str(datetime.datetime.now()))
     start = time.time()
-    if es.indices.exists(index=index_name): # If exists take a snapshot of current docs
-        snapshot_name = index_name + "_snapshot_" + time.strftime("%Y%m%d-%H%M%S")
-        try:
-            es.snapshot.get_repository(repository_name) # Check if repository already was created
-        except: # If not create
-            es.snapshot.create_repository(repository=repository_name, body={"type": "fs", "settings": {"location": backup_directory}})
-        es.snapshot.create(repository=repository_name,snapshot=snapshot_name, body={"indices": index_name})
-        print("Snapshot created: " + snapshot_name)
-        print("See all snapshots with GET <ElasticSearchServerAdress>/_cat/snapshots/openartbrowser_index_backup")
-        print("If fallback to an old snapshot is required close the index with POST <ElasticSearchServerAdress>/" + index_name +"/_close")
-        print("After this apply the snapshot, this will reopen the index POST <ElasticSearchServerAdress>/_snapshot/openartbrowser_index_backup/" + snapshot_name + "/_restore")
-    else:
-        es.indices.create(index=index_name) # Create if index not exists
 
-    update_count = 0
+    print("Checking if index with the name " + index_name + " can be created ...")
+    if(create_empty_index(index_name=index_name)):
+        print("Index " + index_name + " created successfully")
+    else:
+        print("Stopping index creation from file " + file)
+        return
+
     creation_count = 0
-    delete_count = 0
 
     # Document creation
     for item in ijson.items(open(file, 'r', encoding='utf-8'), 'item'):
-        # Search the index by the qid and type (only qId is not unique!)
-        result = es.search(index=index_name, body={"query": {"bool": {"must": [{"match": {"id": item['id']}},{"match": {"type": item['type']}}]}}})
-        result_length = len(result['hits']['hits'])
-        if result_length == 1: # If exists update
-            elastic_search_id = result['hits']['hits'][0]['_id']
-            es.update(id=elastic_search_id, index=index_name, doc_type='data', body={ 'doc': item })
-            update_count += 1
-        elif result_length >= 2: # Remove current if it is a duplicate (sanity check should not occur)
-            es.delete(id=elastic_search_id, index=index_name, doc_type='data')
-            delete_count += 1
-            #raise RuntimeError("There is a duplicate document in the index following qId: " + item['id']) ToDo: Comment in if there are problems with duplicates
-        else:
-            es.create(id=uuid.uuid4(), index=index_name, doc_type='data', body=item)
-            creation_count += 1
+        es.create(id=uuid.uuid4(), index=index_name, doc_type='data', body=item)
+        creation_count += 1
 
     end = time.time()
-    print(str(creation_count) + " indices were created.")
-    print(str(update_count) + " indices were updated.")
-    print(str(delete_count) + " indices were deleted (duplicates).")
-    print("Finished creating/updating the index current time: " +
+    print(str(creation_count) + " documents were created in index " + index_name)
+    print("Finished creating the index current time: " +
         str(datetime.datetime.now()) + " it took " + str((int((end-start)/60))) + " minutes")
 
 
-def list_all_snapshots_from_repository(
-    elastic_search_url='localhost:9200',
-    repository_name='openartbrowser_index_backup'):
+def swap_index(index_name_new, index_name_current, index_name_old) -> bool:
     """
-    Lists all created snapshots in a repository
+    Swaps the new index with the current one the current will
+    be backed up in index_name_old.
+    This is possible because the backup and restore feature of elasticsearch
+    allows renaming when restoring a snapshot.
 
-    :arg elastic_search_url: Url of the ElasticSearch server. On localhost it is 'localhost:9200'
-    :arg repository_name: Name of the repository which contains the snapshots
+    :arg index_name_new: Newly created index which replaces the current index
+    :arg index_name_current: The current index which replaces the old index
+    :arg index_name_old: The old index which will be deleted
+    Returns:
+        True when the index swap worked else False
     """
+    es = Elasticsearch()
+    print("Checking if current index exists")
+    # Check for newly setup ElasticSearch-Server
+    if not es.indices.exists(index=index_name_current):
+        print("The current index named: \"" + index_name_current + 
+        "\" does not exist. It will be created now ...")
+        create_empty_index(index_name=index_name_current)
+    if not es.indices.exists(index=index_name_new):
+        print("The new index named: \"" + index_name_new +
+                "\" does not exist therefore the swap cannot be executed")
+        return False
+
+    print("Creating snapshots from given indices to swap")
+    snapshot_appendix = "_snapshot"
+    index_new_snapshot = index_name_new + snapshot_appendix
+    index_current_snapshot = index_name_current + snapshot_appendix
+    # Snapshot the new index
+    create_snapshot_for_index(
+        index_name=index_name_new,
+        snapshot_name=index_new_snapshot)
+    # Snapshot the current index
+    create_snapshot_for_index(
+        index_name=index_name_current,
+        snapshot_name=index_current_snapshot)
+    list_all_snapshots_from_repository()
+
+    # Close the indices to be swapped, they will be reopened on applying the snapshot
+    es.indices.close(index=index_name_new)
+    es.indices.close(index=index_name_current)
+
+    # First swap
+    # Check if index_name_current_old exists if it does delete index
+    
+    if(es.indices.exists(index_name_old)):
+        es.indices.delete(index_name_old)
+    # Apply snapshot to index_name_current and rename it to index_name_current_old
+    apply_snapshot_from_repository(
+        snapshot_name=index_current_snapshot, 
+        index_name=index_name_current,
+        new_index_name=index_name_old)
+    # Close old index because we not need it right now
+    es.indices.close(index_name_old)
+
+    # Second swap
+    # Apply index_new_snapshot on index_current
+    apply_snapshot_from_repository(
+        snapshot_name=index_new_snapshot,
+        index_name=index_name_new,
+        new_index_name=index_name_current)
+
+    # Now the indices should be swapped
+    # Cleanup
+    # Remove created snapshots
+    delete_snapshot_from_repository(snapshot_name=index_new_snapshot)
+    delete_snapshot_from_repository(snapshot_name=index_current_snapshot)
+
+    # Delete index_name_new
+    delete_index(index_name_new)
+    return True
+
+
+def create_snapshot_for_index(
+    index_name,
+    snapshot_name,
+    repository_name='openartbrowser_index_backup',
+    backup_directory='/var/lib/elasticsearch/backup') -> None:
+    """
+    :arg index_name: Index for which the snapshot should be created for
+    :arg snapshot_name: Name for the snapshot
+    :arg repository_name: Name for a repository which stores snapshots
+                            The openartbrowser repository is 'openartbrowser_index_backup'
+    :arg backup_directory: Directory in which the repository is located
+                            The openartbrowser backup directory is /var/lib/elasticsearch/backup
+                            IMPORTANT:
+                            1. The directory has to exist before execution
+                            2. This directory is also needed in the elasticsearch.yaml configuration file
+                                - Following entry in elasticsearch.yml required:
+                                path.repo: ["path_to_folder"]
+    """
+    es = Elasticsearch()
+
     try:
-        req = requests.get(url="http://" + elastic_search_url + "/_cat/snapshots/" + repository_name)
-        print(req.text)
-    except Exception as e:
-        print(str(e))
+        # Check if repository already was created
+        es.snapshot.get_repository(repository_name)
+    except:  # If not create
+        es.snapshot.create_repository(repository=repository_name, body={
+                                      "type": "fs", "settings": {"location": backup_directory}})
+    if es.indices.exists(index=index_name):
+        es.snapshot.create(repository=repository_name,
+                           snapshot=snapshot_name, 
+                           body={"indices": index_name},
+                           params={"wait_for_completion": "true"})
+        print("Snapshot created: " + snapshot_name)
+    else:
+        print("There is no index with name: " + index_name)
 
 
 def apply_snapshot_from_repository(
     snapshot_name,
-    index_name='api',
-    repository_name='openartbrowser_index_backup'):
+    index_name,
+    new_index_name,
+    repository_name='openartbrowser_index_backup') -> None:
     """
-    Applies a snapshot created in an earlier creation or update from a repository
+    Applies a snapshot created in an earlier creation from a repository
 
     :arg index_name: Name of the index the snapshot should be applied
     :arg repository_name: Name of the repository the snapshot is in
     :arg snapshot_name: Name of the snapshot
     """
     es = Elasticsearch()
+
     try:
         es.indices.close(index=index_name)
-        result = es.snapshot.restore(repository=repository_name, snapshot=snapshot_name)
+        result = es.snapshot.restore(
+            repository=repository_name, 
+            snapshot=snapshot_name,
+            body={
+                "indices" : index_name,
+                "rename_pattern" : index_name,
+                "rename_replacement": new_index_name
+            },
+            params={
+                "wait_for_completion": "true",
+            })
         print(str(result))
+    except Exception as e:
+        print(str(e))
+
+
+def delete_snapshot_from_repository(
+        snapshot_name,
+        repository_name='openartbrowser_index_backup',
+        backup_directory='/var/lib/elasticsearch/backup') -> None:
+    """
+    Delete a snapshot from the repository
+
+    :arg snapshot_name: Name of the snapshot to be deleted.
+    :arg repository_name: Name of the repository the snapshot is in
+    :arg backup_directory: Directory in which the repository is located.
+                           See create_snapshot_for_index for more information on that.
+    """
+    es = Elasticsearch()
+
+    try:
+        # Check if repository already was created
+        es.snapshot.get_repository(repository_name)
+    except:  # If not create
+        es.snapshot.create_repository(repository=repository_name, body={
+                                      "type": "fs", "settings": {"location": backup_directory}})
+    try:
+        es.snapshot.delete(repository=repository_name, 
+                           snapshot=snapshot_name)
+    except Exception as e:
+        print("There was a problem deleting the snapshot:")
+        print(str(e))
+
+
+def list_all_snapshots_from_repository(
+        elastic_search_url='localhost:9200',
+        repository_name='openartbrowser_index_backup') -> None:
+    """
+    Lists all created snapshots in a repository
+
+    :arg elastic_search_url: Url of the ElasticSearch server. The default is 'localhost:9200'.
+    :arg repository_name: Name of the repository which contains the snapshots
+    """
+    try:
+        req = requests.get(url="http://" + elastic_search_url +
+                           "/_cat/snapshots/" + repository_name)
+        print(req.text)
+    except Exception as e:
+        print(str(e))
+
+
+def list_all_indices(elastic_search_url='localhost:9200') -> None:
+    """
+    List all indices
+
+    :arg elastic_search_url: Url of the ElasticSearch server. The default is 'localhost:9200'.
+    """
+    try:
+        req = requests.get(url="http://" + elastic_search_url +
+                           "/_cat/indices")
+        print(req.text)
     except Exception as e:
         print(str(e))
 
 
 def language_config_to_list(
     config_file=Path(__file__).parent.parent.absolute() /
-    "languageconfig.csv"
-):
+    "languageconfig.csv"):
     """[Reads languageconfig.csv and returns array that contains its
     full contents]
 
@@ -133,12 +303,63 @@ def language_config_to_list(
                 languageValues.append(row)
     return languageValues
 
-def create_or_update_for_each_language(lang_keys=[item[0] for item in language_config_to_list()], filepath=Path(__file__).resolve().parent.parent / "crawler_output"):
+
+def create_index_for_each_language(
+    lang_keys=[item[0] for item in language_config_to_list()], 
+    filepath=Path(__file__).resolve().parent.parent / "crawler_output") -> None:
+    """
+    Creates a new index for each language in the languageconfig.csv.
+    The new indice name convention is <indexname>_new
+
+    :arg lang_leys: Languagekeys for which the index has to be created
+    :arg filepath: Location of the art_ontology_*.json language files
+    """
     for key in lang_keys:
-        create_or_update_index(file=filepath / str("art_ontology_" + key + ".json"),index_name=key)
+        create_index(file=filepath / str("art_ontology_" + key + ".json"),index_name=key + "_new")
+
+
+def swap_index_for_each_language(lang_keys=[item[0] for item in language_config_to_list()]) -> None:
+    """
+    Swaps a newly created index (identified by it's name *_new) with the current one.
+    The current index is saved to *_old.
+
+    :arg lang_leys: Languagekeys for which the index has to be created.
+    """
+    for key in lang_keys:
+        swap_index(index_name_new=key + "_new", index_name_current=key, index_name_old=key + "_old")
+
+
+def swap_to_backup_for_each_language(
+    lang_keys=[item[0] for item in language_config_to_list()],
+    delete_non_working_indices=True) -> None:
+    """
+    Can be used if the current indices aren't working. This resets the indices to the <languagecode>_old
+    indices. The current non working indices can be saved to an index with the restoration date as name.
+    This is only possible once because of the renaming of the indices <languagecode>_old to <languagecode>.
+
+    :arg lang_leys: Languagekeys for which the index has to be created.
+    :arg delete_non_working_indices: If this is set to True the non working indices will be deleted.
+                                     For debugging purposes set to this False.
+    """
+    es = Elasticsearch()
+    for key in lang_keys:
+        not_working_index_name = key + time.strftime("%Y%m%d-%H%M%S")
+        es.indices.open(key + "_old") # open old index for reapplying
+        if swap_index(index_name_new=key + "_old", index_name_current=key,
+                   index_name_old=not_working_index_name):
+            if(delete_non_working_indices):
+                print("The not working index named " + not_working_index_name + " will now be deleted")
+                delete_index(not_working_index_name)
+            else:
+                print("The not working index " + key + " was renamed to " + 
+                not_working_index_name + " and can now be debugged")
+                print("After debugging deletion is possible with the delete_index function")
+
 
 if __name__ == "__main__":
-#    if len(sys.argv) > 0:
-#        filepath = sys.argv[1]
-#        create_or_update_index(index_name='api', file=filepath)
-    create_or_update_for_each_language()
+    create_index_for_each_language()
+    swap_index_for_each_language()
+
+    # This can be used if the newly added indices aren't working 
+    # and you want to go back to <languagecode>_old for example
+    #swap_to_backup_for_each_language()

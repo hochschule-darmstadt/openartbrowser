1ca6e6c839bd0a65e375eb386aea7e2ad1842801
mauamy <yannick.mau@stud.h-da.de>
2019-12-17 17:26:57 +0100

    Add new test case and fix errors thrown by the old one.

--- app/src/app/shared/components/timeline/timeline.component.spec.ts
+++ app/src/app/shared/components/timeline/timeline.component.spec.ts
@@ -6,6 +6,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {TimelineComponent} from './timeline.component';
 import {Ng5SliderModule} from 'ng5-slider';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('SliderComponent', () => {
   let component: TimelineComponent;
@@ -17,7 +18,8 @@ describe('SliderComponent', () => {
         NgbModule,
         Ng5SliderModule,
         HttpClientModule,
-        RouterModule.forRoot([])
+        RouterModule.forRoot([]),
+        BrowserAnimationsModule,
       ],
       declarations: [
         TimelineComponent
@@ -34,7 +36,50 @@ describe('SliderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create without items', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create with items', () => {
+    component.items = [
+     {
+       id: 'Q18578798',
+       label: 'The Execution of the Four Crowned Martyrs',
+       description: 'painting by Niccolò di Pietro Gerini',
+       type: 'artwork',
+       absoluteRank: 37,
+       relativeRank: 0.9795063116489386,
+       date: 1300
+     },
+     {
+       id: 'Q549172',
+       label: 'Flight into Egypt',
+       description: 'fresco by Giotto',
+       type: 'artwork',
+       absoluteRank: 46,
+       relativeRank: 0.9944890206318238,
+       date: 1305
+     },
+     {
+       id: 'Q979440',
+       label: 'Annunciation with St. Margaret and St. Ansanus',
+       description: 'painting by Simone Martini and Lippo Memmi',
+       type: 'artwork',
+       absoluteRank: 36,
+       relativeRank: 0.9762525340301427,
+       date: 1333
+     },
+     {
+       id: 'Q3815314',
+       label: 'Head of Christ',
+       description: 'sculpture by Jaume Cascalls',
+       abstract: 'The Head of Christ is a Jesus head conserved at the National Art Museum of Catalonia.',
+       type: 'artwork',
+       absoluteRank: 21,
+       relativeRank: 0.22987683344390347,
+       date: 1352,
+     },
+   ];
     expect(component).toBeTruthy();
   });
 });

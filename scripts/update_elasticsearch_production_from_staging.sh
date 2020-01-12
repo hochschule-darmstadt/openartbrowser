6dadffd491f557875135a170d53480c4d81f40b7
Tilo Michel <tilo.w.michel@stud.h-da.de>
2020-01-12 21:00:57 +0100

    Remove old archive from last run

--- scripts/update_elasticsearch_production_from_staging.sh
+++ scripts/update_elasticsearch_production_from_staging.sh
@@ -3,6 +3,10 @@ if [ $(id -u) -ne 0 ]
 	then echo "The script must be executed with sudo rights"
 	exit 1
 fi
+
+# Remove old archive from last run -f avoid error when file is not existing
+rm -f crawler_output.tar.gz
+
 # Get crawler_output from staging
 wget -4 http://cai-artbrowserstaging.fbi.h-da.de/crawler_output.tar.gz
 

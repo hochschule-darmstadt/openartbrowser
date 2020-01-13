4b230c0ba736303f61b5cbb0556e4bf2d6fe260d
lbucas <luc.b1197@gmail.com>
2020-01-13 15:44:49 +0100

    Refactor language dropdown code & event tracking: Languages

--- app/src/app/core/components/header/header.component.ts
+++ app/src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, LOCALE_ID, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import {Angulartics2} from "angulartics2";
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,18 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   public path: string = "";
   public locale: string = ""
-  
-  constructor(private router: Router, @Inject(LOCALE_ID) protected localeId: string) {
+
+  public languages = [
+    {code: 'en', name: 'English'},
+    {code: 'de', name: 'Deutsch'},
+    {code: 'fr', name: 'Français'},
+    {code: 'es', name: 'Español'},
+    {code: 'it', name: 'Italiano'},
+  ];
+
+  constructor(private router: Router, @Inject(LOCALE_ID) protected localeId: string, private angulartics2: Angulartics2) {
     /**
-     * Set the variable path to router url 
+     * Set the variable path to router url
      * every time the url changed.
      */
     router.events.subscribe(val => this.path = this.router.url);

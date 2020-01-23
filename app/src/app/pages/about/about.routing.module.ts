585a6a2ed09b30c138e1fc2b3bc7ceb7038525b5
BaderChouaib <bdr.chouaib@gmail.com>
2020-01-23 11:11:28 +0100

    code review about changes by prettier

--- app/src/app/pages/about/about.routing.module.ts
+++ app/src/app/pages/about/about.routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { SharedModule } from '../../shared/shared.module';
-import { AboutComponent } from './about.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { SharedModule } from "../../shared/shared.module";
+import { AboutComponent } from "./about.component";
 
-const aboutnRoutes: Routes = [
-    {
-        path: '',
-        component: AboutComponent,
-    },
+const aboutRoutes: Routes = [
+  {
+    path: "",
+    component: AboutComponent
+  }
 ];
 
 @NgModule({
-    declarations: [AboutComponent],
-    imports: [CommonModule, SharedModule, RouterModule.forChild(aboutnRoutes)],
-    exports: [AboutComponent],
+  declarations: [AboutComponent],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(aboutRoutes)],
+  exports: [AboutComponent]
 })
-export class AboutRoutingModule { }
+export class AboutRoutingModule {}

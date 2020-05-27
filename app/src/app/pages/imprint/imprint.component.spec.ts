9119682aade6daadac529fe9eb6a2b691660f9e0
mxxbe <max-belling@web.de>
2020-05-27 17:50:35 +0200

    renamed impress->imprint #311

--- app/src/app/pages/imprint/imprint.component.spec.ts
+++ app/src/app/pages/imprint/imprint.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ImpressComponent } from './impress.component';
+import { ImpressComponent } from './imprint.component';
 
 describe('ImpressComponent', () => {
   let component: ImpressComponent;

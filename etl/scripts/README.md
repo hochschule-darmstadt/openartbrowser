8ffb97ebadd2e2ff5796beac8274234213a86a90
Tilo Michel <tilo.w.michel@stud.h-da.de>
2020-06-01 13:30:19 +0200

    Adjustments to env var export
    + Changed README + install scripts

--- etl/scripts/README.md
+++
@@ -1,1 +0,0 @@
-:warning: These scripts should all be executed from the parent directory (etl) otherwise they won't work e. g. `./scripts/run_etl.sh`
--- etl/scripts/install_etl.sh
+++ etl/scripts/install_etl.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 echo "export PYTHONPATH="${PYTHONPATH}:${pwd}"" >> ~/.bashrc # Add directory to PYTHONPATH, this enables imports in python3
 echo "export PYWIKIBOT_DIR="${pwd}"" >> ~/.bashrc # Add directory to PYWIKIBOT_DIR, this enables execution of data_extraction scripts in any directory
+pip3 install -r requirements.txt
 cd data_manipulation
 npm install

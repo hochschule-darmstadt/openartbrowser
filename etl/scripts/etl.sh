d26cb15386831b1cf4f840f59bf48c5d84235240
Tilo Michel <tilo.w.michel@stud.h-da.de>
2020-06-11 21:30:13 +0200

    Change reference manipulation to enhancement

--- etl/scripts/etl.sh
+++ etl/scripts/etl.sh
@@ -20,26 +20,26 @@ python3 data_extraction/get_wikipedia_extracts.py
 
 cd crawler_output/intermediate_files/json/
 
-node --max-old-space-size=4096 ../../../data_manipulation/script_artworks_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_artworks_rank.js
 
-node --max-old-space-size=4096 ../../../data_manipulation/script_genres_rank.js
-node --max-old-space-size=4096 ../../../data_manipulation/script_artist_rank.js
-node --max-old-space-size=4096 ../../../data_manipulation/script_locations_rank.js
-node --max-old-space-size=4096 ../../../data_manipulation/script_materials_rank.js
-node --max-old-space-size=4096 ../../../data_manipulation/script_movements_rank.js
-node --max-old-space-size=4096 ../../../data_manipulation/script_motifs_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_genres_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_artist_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_locations_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_materials_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_movements_rank.js
+node --max-old-space-size=4096 ../../../data_enhancement/script_motifs_rank.js
 
 # Merges all *_rank.json files into art_ontology.json
-node --max-old-space-size=4096 ../../../data_manipulation/merge_art_data.js
+node --max-old-space-size=4096 ../../../data_enhancement/merge_art_data.js
 
 rm -f ../../../crawler_output/art_ontology.json
 
 # Move the generated art_ontology.json to the directory crawler_output
 mv art_ontology.json ../../../crawler_output/art_ontology.json
 
-python3 ../../../data_manipulation/add_youtube_videos.py
+python3 ../../../data_enhancement/add_youtube_videos.py
 
-python3 ../../../data_manipulation/split_languages.py
+python3 ../../../data_enhancement/split_languages.py
 
 python3 ../../../upload_to_elasticsearch/elasticsearch_helper.py
 

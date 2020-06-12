76e6268126caea2df5e80c9097643a0dea9e72a9
EeveesEyes <adrian.v.kailus@stud.h-da.de>
2020-06-12 13:25:12 +0200

    #296: reverted changes to routing module ( local routing fails )

--- app/src/app/app.routing.module.ts
+++ app/src/app/app.routing.module.ts
@@ -8,45 +8,32 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const enum RoutingModules {
-  HomeRoutingModule = './pages/home/home.routing.module#HomeRoutingModule',
-  ImprintRoutingModule = './pages/imprint/imprint.routing.module#ImprintRoutingModule',
-  DataProtectionRoutingModule = './pages/data-protection/data-protection.routing.module#DataProtectionRoutingModule',
-  AboutRoutingModule = './pages/about/about.routing.module#AboutRoutingModule',
-  EntitiesRoutingModule = './pages/entities/entities.routing.module#EntitiesRoutingModule',
-  ArtistRoutingModule = './pages/artist/artist.routing.module#ArtistRoutingModule',
-  ArtworkRoutingModule = './pages/artwork/artwork.routing.module#ArtworkRoutingModule',
-  GenreRoutingModule = './pages/genre/genre.routing.module#GenreRoutingModule',
-  LocationRoutingModule = './pages/location/location.routing.module#LocationRoutingModule',
-  MaterialRoutingModule = './pages/material/material.routing.module#MaterialRoutingModule',
-  MovementRoutingModule = './pages/movement/movement.routing.module#MovementRoutingModule',
-  MotifRoutingModule = './pages/motif/motif.routing.module#MotifRoutingModule',
-  SearchResultRoutingModule = './pages/search-result/search-result.routing.module#SearchResultRoutingModule',
-}
-
 /** routes to our feature modules.
  * advantage of routing to modules instead of components: lazy loading.
  */
 const routes: Routes = [
-  { path: '', loadChildren: RoutingModules.HomeRoutingModule },
-  { path: 'imprint', loadChildren: RoutingModules.ImprintRoutingModule },
-  { path: 'data-protection', loadChildren: RoutingModules.DataProtectionRoutingModule },
-  { path: 'about', loadChildren: RoutingModules.AboutRoutingModule },
-  { path: 'movements', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'artists', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'artworks', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'genres', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'motifs', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'locations', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'materials', loadChildren: RoutingModules.EntitiesRoutingModule },
-  { path: 'artist/:artistId', loadChildren: RoutingModules.ArtistRoutingModule },
-  { path: 'artwork/:artworkId', loadChildren: RoutingModules.ArtworkRoutingModule },
-  { path: 'genre/:genreId', loadChildren: RoutingModules.GenreRoutingModule },
-  { path: 'location/:locationId', loadChildren: RoutingModules.LocationRoutingModule },
-  { path: 'material/:materialId', loadChildren: RoutingModules.MaterialRoutingModule },
-  { path: 'movement/:movementId', loadChildren: RoutingModules.MovementRoutingModule },
-  { path: 'motif/:motifId', loadChildren: RoutingModules.MotifRoutingModule },
-  { path: 'search', loadChildren: RoutingModules.SearchResultRoutingModule },
+  { path: '', loadChildren: './pages/home/home.routing.module#HomeRoutingModule' },
+  { path: 'imprint', loadChildren: './pages/imprint/imprint.routing.module#ImprintRoutingModule' },
+  {
+    path: 'data-protection',
+    loadChildren: './pages/data-protection/data-protection.routing.module#DataProtectionRoutingModule'
+  },
+  { path: 'about', loadChildren: './pages/about/about.routing.module#AboutRoutingModule' },
+  { path: 'movements', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'artists', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'artworks', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'genres', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'motifs', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'locations', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'materials', loadChildren: './pages/entities/entities.routing.module#EntitiesRoutingModule' },
+  { path: 'artist/:artistId', loadChildren: './pages/artist/artist.routing.module#ArtistRoutingModule' },
+  { path: 'artwork/:artworkId', loadChildren: './pages/artwork/artwork.routing.module#ArtworkRoutingModule' },
+  { path: 'genre/:genreId', loadChildren: './pages/genre/genre.routing.module#GenreRoutingModule' },
+  { path: 'location/:locationId', loadChildren: './pages/location/location.routing.module#LocationRoutingModule' },
+  { path: 'material/:materialId', loadChildren: './pages/material/material.routing.module#MaterialRoutingModule' },
+  { path: 'movement/:movementId', loadChildren: './pages/movement/movement.routing.module#MovementRoutingModule' },
+  { path: 'motif/:motifId', loadChildren: './pages/motif/motif.routing.module#MotifRoutingModule' },
+  { path: 'search', loadChildren: './pages/search-result/search-result.routing.module#SearchResultRoutingModule' },
   { path: '**', redirectTo: '' }
 ];
 

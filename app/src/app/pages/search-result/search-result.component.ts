671d9e2043b40557ae13066cf1b2882a60c09f7e
Fablicious <burillof@hotmail.de>
2021-06-13 15:13:36 +0200

    #505 Added 'All'-tab to search result page. Other tabs in progress

--- app/src/app/pages/search-result/search-result.component.ts
+++ app/src/app/pages/search-result/search-result.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Artwork, EntityType, Entity, EntityIcon, ArtSearch } from 'src/app/shared/models/models';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ArtSearch, Artwork, Entity, EntityIcon, EntityType } from 'src/app/shared/models/models';
 import { ActivatedRoute } from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
@@ -10,12 +10,19 @@ import { usePlural } from 'src/app/shared/models/entity.interface';
 /**
  * @description Interface for the search results.
  * @export
- * @interface SearchResult
  */
-export interface SearchResult {
+export interface SearchObject {
   items: Entity[];
   key: EntityType;
   icon: EntityIcon;
+}
+
+/** interface for the tabs */
+interface EntityTab {
+  type: EntityType;
+  itemsFound: boolean; // boolean if searchResults exist for this EntityType
+  icon: EntityIcon;
+  active: boolean;
 }
 
 @Component({
@@ -25,63 +32,66 @@ export interface SearchResult {
 })
 export class SearchResultComponent implements OnInit, OnDestroy {
   /** Related artworks */
-  sliderItems: Artwork[] = [];
+  searchResults: Artwork[] = [];
+
+  /**
+   * @description for the tabs in slider/carousel.
+   */
+  entityTabs: EntityTab[] = [];
 
   /**
    * variable of the search results
-   * @type {SearchResult[]}
-   * @memberof SearchResultComponent
    */
-  searchResults: SearchResult[] = [];
+  searchObjects: SearchObject[] = [];
 
   /**
    * specific search terms
-   * @type {string[]}
-   * @memberof SearchResultComponent
    */
   searchTerms: string[] = [];
 
   /** use this to end subscription to url parameter in ngOnDestroy */
   private ngUnsubscribe = new Subject();
 
-  constructor(private dataService: DataService, private route: ActivatedRoute, private angulartics2: Angulartics2) {}
+  constructor(private dataService: DataService, private route: ActivatedRoute, private angulartics2: Angulartics2) {
+  }
 
   /** hook that is executed at component initialization */
   ngOnInit() {
     /** Extract the search params from url query params. */
     this.route.queryParams.pipe(takeUntil(this.ngUnsubscribe)).subscribe(async params => {
       /** resets search results  */
-      this.searchResults = [];
+      this.searchObjects = [];
       this.searchTerms = params.term ? (Array.isArray(params.term) ? params.term : [params.term]) : [];
       if (params.artist) {
-        this.searchResults.push(await this.getSearchResults(params.artist, EntityType.ARTIST, EntityIcon.ARTIST));
+        this.searchObjects.push(await this.getSearchResults(params.artist, EntityType.ARTIST, EntityIcon.ARTIST));
       }
       if (params.movement) {
-        this.searchResults.push(await this.getSearchResults(params.movement, EntityType.MOVEMENT, EntityIcon.MOVEMENT));
+        this.searchObjects.push(await this.getSearchResults(params.movement, EntityType.MOVEMENT, EntityIcon.MOVEMENT));
       }
       if (params.genre) {
-        this.searchResults.push(await this.getSearchResults(params.genre, EntityType.GENRE, EntityIcon.GENRE));
+        this.searchObjects.push(await this.getSearchResults(params.genre, EntityType.GENRE, EntityIcon.GENRE));
       }
       if (params.motif) {
-        this.searchResults.push(await this.getSearchResults(params.motif, EntityType.MOTIF, EntityIcon.MOTIF));
+        this.searchObjects.push(await this.getSearchResults(params.motif, EntityType.MOTIF, EntityIcon.MOTIF));
       }
       if (params.location) {
-        this.searchResults.push(await this.getSearchResults(params.location, EntityType.LOCATION, EntityIcon.LOCATION));
+        this.searchObjects.push(await this.getSearchResults(params.location, EntityType.LOCATION, EntityIcon.LOCATION));
       }
       if (params.material) {
-        this.searchResults.push(await this.getSearchResults(params.material, EntityType.MATERIAL, EntityIcon.MATERIAL));
+        this.searchObjects.push(await this.getSearchResults(params.material, EntityType.MATERIAL, EntityIcon.MATERIAL));
       }
-      this.sliderItems = await this.getSliderItems(this.searchResults, this.searchTerms);
+      this.searchResults = await this.getSliderItems(this.searchObjects, this.searchTerms);
 
       this.angulartics2.eventTrack.next({
         action: 'trackSiteSearch',
         properties: {
           category: 'Search page',
           keyword: this.searchTerms.toString(),
-          searchCount: this.sliderItems.length
+          searchCount: this.searchResults.length
         }
       });
     });
+    this.addTab(EntityType.ALL, true);
   }
 
   /**
@@ -90,7 +100,7 @@ export class SearchResultComponent implements OnInit, OnDestroy {
    * @param type filter by type
    * @param icon icon for SearchResult.
    */
-  private async getSearchResults<T>(ids: [], type: EntityType, icon: EntityIcon): Promise<SearchResult> {
+  private async getSearchResults<T>(ids: [], type: EntityType, icon: EntityIcon): Promise<SearchObject> {
     const items = (await this.dataService.findMultipleById<T>([].concat(ids), type)) as any[];
     return { items, icon, key: type };
   }
@@ -101,10 +111,29 @@ export class SearchResultComponent implements OnInit, OnDestroy {
    * @param results search result array
    * @param terms terms array
    */
-  private async getSliderItems(results: SearchResult[], terms: string[]): Promise<Artwork[]> {
+  private async getSliderItems(results: SearchObject[], terms: string[]): Promise<Artwork[]> {
     const search: ArtSearch = {};
     results.forEach(typeArray => (search[usePlural(typeArray.key)] = typeArray.items.map((e: Entity) => e.id)));
     return await this.dataService.searchArtworks(search, terms);
+  }
+
+  /**
+   * Add tab to entity tab array
+   * @param type Tab title
+   * @param itemsFound If Items where found in elastic search
+   * @param active Is active tab
+   */
+  private addTab(type: EntityType, itemsFound: boolean = false, active: boolean = false) {
+    this.entityTabs.push({
+      active,
+      icon: EntityIcon[type.toUpperCase()],
+      type,
+      itemsFound
+    });
+  }
+
+  tabClicked(tabClicked: EntityTab) {
+
   }
 
   ngOnDestroy() {
@@ -112,3 +141,4 @@ export class SearchResultComponent implements OnInit, OnDestroy {
     this.ngUnsubscribe.complete();
   }
 }
+

e5ed9e603ddb1e56dd72d3a32a0965daf579bfa7
Fablicious <burillof@hotmail.de>
2021-06-15 18:09:05 +0200

    #505 Other tabs are displayed depending on countEndpoint result

--- app/src/app/pages/search-result/search-result.component.ts
+++ app/src/app/pages/search-result/search-result.component.ts
@@ -20,7 +20,6 @@ export interface SearchObject {
 /** interface for the tabs */
 interface EntityTab {
   type: EntityType;
-  itemsFound: boolean; // boolean if searchResults exist for this EntityType
   icon: EntityIcon;
   active: boolean;
 }
@@ -63,24 +62,24 @@ export class SearchResultComponent implements OnInit, OnDestroy {
       this.searchObjects = [];
       this.searchTerms = params.term ? (Array.isArray(params.term) ? params.term : [params.term]) : [];
       if (params.artist) {
-        this.searchObjects.push(await this.getSearchResults(params.artist, EntityType.ARTIST, EntityIcon.ARTIST));
+        this.searchObjects.push(await this.getSearchObjects(params.artist, EntityType.ARTIST, EntityIcon.ARTIST));
       }
       if (params.movement) {
-        this.searchObjects.push(await this.getSearchResults(params.movement, EntityType.MOVEMENT, EntityIcon.MOVEMENT));
+        this.searchObjects.push(await this.getSearchObjects(params.movement, EntityType.MOVEMENT, EntityIcon.MOVEMENT));
       }
       if (params.genre) {
-        this.searchObjects.push(await this.getSearchResults(params.genre, EntityType.GENRE, EntityIcon.GENRE));
+        this.searchObjects.push(await this.getSearchObjects(params.genre, EntityType.GENRE, EntityIcon.GENRE));
       }
       if (params.motif) {
-        this.searchObjects.push(await this.getSearchResults(params.motif, EntityType.MOTIF, EntityIcon.MOTIF));
+        this.searchObjects.push(await this.getSearchObjects(params.motif, EntityType.MOTIF, EntityIcon.MOTIF));
       }
       if (params.location) {
-        this.searchObjects.push(await this.getSearchResults(params.location, EntityType.LOCATION, EntityIcon.LOCATION));
+        this.searchObjects.push(await this.getSearchObjects(params.location, EntityType.LOCATION, EntityIcon.LOCATION));
       }
       if (params.material) {
-        this.searchObjects.push(await this.getSearchResults(params.material, EntityType.MATERIAL, EntityIcon.MATERIAL));
+        this.searchObjects.push(await this.getSearchObjects(params.material, EntityType.MATERIAL, EntityIcon.MATERIAL));
       }
-      this.searchResults = await this.getSliderItems(this.searchObjects, this.searchTerms);
+      this.searchResults = await this.getSearchResults(this.searchObjects, this.searchTerms);
 
       this.angulartics2.eventTrack.next({
         action: 'trackSiteSearch',
@@ -91,7 +90,21 @@ export class SearchResultComponent implements OnInit, OnDestroy {
         }
       });
     });
+    // All tab should be added by default and set active
     this.addTab(EntityType.ALL, true);
+
+    /**
+     * Look in elasticSearch if results exist for current query and depending on result add a tab for it
+     */
+    Object.values(EntityType).forEach(async type => {
+      if (type === 'all') { // skip all-type
+        return;
+      }
+      const results = await this.getSearchResults(this.searchObjects, this.searchTerms, true, type);
+      if (results) {
+        this.addTab(type);
+      }
+    });
   }
 
   /**
@@ -100,7 +113,7 @@ export class SearchResultComponent implements OnInit, OnDestroy {
    * @param type filter by type
    * @param icon icon for SearchResult.
    */
-  private async getSearchResults<T>(ids: [], type: EntityType, icon: EntityIcon): Promise<SearchObject> {
+  private async getSearchObjects<T>(ids: [], type: EntityType, icon: EntityIcon): Promise<SearchObject> {
     const items = (await this.dataService.findMultipleById<T>([].concat(ids), type)) as any[];
     return { items, icon, key: type };
   }
@@ -111,10 +124,14 @@ export class SearchResultComponent implements OnInit, OnDestroy {
    * @param results search result array
    * @param terms terms array
    */
-  private async getSliderItems(results: SearchObject[], terms: string[]): Promise<Artwork[]> {
+  private async getSearchResults(results: SearchObject[], terms: string[], returnCountOnly = false, type = null): Promise<Artwork[]> {
     const search: ArtSearch = {};
     results.forEach(typeArray => (search[usePlural(typeArray.key)] = typeArray.items.map((e: Entity) => e.id)));
-    return await this.dataService.searchArtworks(search, terms);
+    if (returnCountOnly) {
+      return await this.dataService.countSearchResultItems(search, terms, type);
+    } else {
+      return await this.dataService.searchArtworks(search, terms);
+    }
   }
 
   /**
@@ -123,12 +140,11 @@ export class SearchResultComponent implements OnInit, OnDestroy {
    * @param itemsFound If Items where found in elastic search
    * @param active Is active tab
    */
-  private addTab(type: EntityType, itemsFound: boolean = false, active: boolean = false) {
+  private addTab(type: EntityType, active: boolean = false) {
     this.entityTabs.push({
       active,
       icon: EntityIcon[type.toUpperCase()],
-      type,
-      itemsFound
+      type
     });
   }
 
